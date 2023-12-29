6c9ee7ea72f57e685682e17639792db103afa622
bimalitani100 <116113697+bimalitani100@users.noreply.github.com>
2023-12-29 00:05:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+var button= document.getElementById("myButton");
+button.addEventListener("click", function(){
+    var linkToGo="https://github.com/bimalitani100/project-1";
+    window.location.href= linkToGo;
+});
